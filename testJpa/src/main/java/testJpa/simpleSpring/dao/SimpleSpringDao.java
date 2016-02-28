55a7e042fb4022118c92830a0e4c4c7607f15443
Christian Schaefer <chr.j.schaefer@web.de>
2016-02-28 01:28:32 +0100

    remove the redundant method declarations

--- testJpa/src/main/java/testJpa/simpleSpring/dao/SimpleSpringDao.java
+++ testJpa/src/main/java/testJpa/simpleSpring/dao/SimpleSpringDao.java
@@ -1,7 +1,5 @@
 package testJpa.simpleSpring.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,64 +11,6 @@ import testJpa.simpleSpring.domain.SimpleSpring;
  */
 @Repository
 public interface SimpleSpringDao extends JpaRepository<SimpleSpring, Long>, SimpleSpringDaoCustom {
-
-    /**
-     * save an entity
-     *
-     * @param entity
-     *            to save
-     * @return the managed entity
-     */
-    @Override
-    SimpleSpring save(SimpleSpring entity);
-
-    /**
-     * find one entity by id
-     *
-     * @param id
-     *            to find
-     * @return the entity
-     */
-    @Override
-    SimpleSpring findOne(Long id);
-
-    /**
-     * Retrieve all entities from the table. No specific order is guaranteed.
-     *
-     * @return all entities
-     */
-    @Override
-    List<SimpleSpring> findAll();
-
-    /**
-     * Get count of entities in the table.
-     * <p>
-     * Use {@link SimpleSpringDao#isEmpty()} instead if count would be check for
-     * zero only.
-     *
-     * @return count of entities in the table
-     */
-    @Override
-    long count();
-
-    /**
-     * delete one entity
-     *
-     * @param entity
-     *            to delete
-     */
-    @Override
-    void delete(SimpleSpring entity);
-
-    /**
-     * check if one entity exists in the table
-     *
-     * @param id
-     *            to find
-     * @return true if entity exists
-     */
-    @Override
-    boolean exists(Long id);
 
     /**
      * find by data field
