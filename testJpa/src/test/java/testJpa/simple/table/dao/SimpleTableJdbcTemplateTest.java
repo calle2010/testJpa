3bab4541958b772272fbf7887ea449cb2ee7981a
Christian Schaefer <chr.j.schaefer@web.de>
2016-03-08 00:14:48 +0100

    rename test cases for JdbcTemplate

--- testJpa/src/test/java/testJpa/simple/table/dao/SimpleTableJdbcTemplateTest.java
+++ testJpa/src/test/java/testJpa/simple/table/dao/SimpleTableJdbcTemplateTest.java
@@ -1,4 +1,4 @@
-package testJpa.spring.table.dao;
+package testJpa.simple.table.dao;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -26,20 +26,24 @@ import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.transaction.annotation.Transactional;
 
 import testJpa.TestJpaTestConfiguration;
-import testJpa.spring.table.domain.SpringTable;
+import testJpa.simple.table.domain.SimpleTable;
 
 /**
  * Test CRUD functionality of a simple table without relationships.
+ * <p>
+ * This class uses {@link JdbcTemplate} for database setup and verification of
+ * results. All changes are rolled back at the end of a test method.
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestJpaTestConfiguration.class)
 @Transactional
-public class SimpleSpringTestTransactionTest {
+public class SimpleTableJdbcTemplateTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleSpringTestTransactionTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleTableJdbcTemplateTest.class);
 
     @Autowired
-    SpringTableDao dao;
+    SimpleTableDao dao;
 
     @PersistenceContext
     EntityManager em;
@@ -58,32 +62,32 @@ public class SimpleSpringTestTransactionTest {
     @Test
     public void testCount() {
 
-        setupSimpleSpring();
+        setupSimpleTable();
 
         assertEquals(3, dao.count());
     }
 
-    private void setupSimpleSpring() {
+    private void setupSimpleTable() {
 
         assertTrue("transaction must be active", TestTransaction.isActive());
         assertTrue("transaction is set to rollback", TestTransaction.isFlaggedForRollback());
 
-        jdbc.update("delete from SPRING_TABLE");
+        jdbc.update("delete from SIMPLE_TABLE");
 
-        jdbc.update("insert into SPRING_TABLE values(?, ?)", 10001000l, "one thousand");
-        jdbc.update("insert into SPRING_TABLE values(?, ?)", 10001002l, "one thousand two");
-        jdbc.update("insert into SPRING_TABLE values(?, ?)", 10001001l, "one thousand one");
+        jdbc.update("insert into SIMPLE_TABLE values(?, ?)", 10001000l, "one thousand");
+        jdbc.update("insert into SIMPLE_TABLE values(?, ?)", 10001002l, "one thousand two");
+        jdbc.update("insert into SIMPLE_TABLE values(?, ?)", 10001001l, "one thousand one");
 
     }
 
     @Test
     public void testCreate() {
-        setupSimpleSpring();
+        setupSimpleTable();
 
-        final SpringTable st = new SpringTable();
+        final SimpleTable st = new SimpleTable();
         st.setData("new entry");
 
-        final SpringTable stPersisted = dao.save(st);
+        final SimpleTable stPersisted = dao.save(st);
 
         assertNotEquals(0, stPersisted.getId().longValue());
 
@@ -91,37 +95,36 @@ public class SimpleSpringTestTransactionTest {
         // statements for insert
         em.flush();
 
-        assertEquals(4, jdbc.queryForObject("select count(ID) from SPRING_TABLE", Integer.class));
+        assertEquals(4, jdbc.queryForObject("select count(ID) from SIMPLE_TABLE", Integer.class));
 
-        assertEquals(1, jdbc.queryForList("select id from SPRING_TABLE where data = 'new entry'").size());
-
+        assertEquals(1, jdbc.queryForList("select id from SIMPLE_TABLE where data = 'new entry'").size());
     }
 
     @Test
     public void testExists() {
-        setupSimpleSpring();
+        setupSimpleTable();
 
         assertTrue(dao.exists(10001000l));
     }
 
     @Test
     public void testExistsFailing() {
-        setupSimpleSpring();
+        setupSimpleTable();
         assertFalse(dao.exists(999l));
     }
 
     @Test
     public void testFindAll() {
-        setupSimpleSpring();
-        final List<SpringTable> list = dao.findAll();
+        setupSimpleTable();
+        final List<SimpleTable> list = dao.findAll();
 
         assertEquals(3, list.size());
     }
 
     @Test
     public void testFindByData() {
-        setupSimpleSpring();
-        final List<SpringTable> list = dao.findByData("one thousand");
+        setupSimpleTable();
+        final List<SimpleTable> list = dao.findByData("one thousand");
 
         assertEquals(1, list.size());
         assertEquals(10001000, list.get(0).getId().longValue());
@@ -129,29 +132,29 @@ public class SimpleSpringTestTransactionTest {
 
     @Test
     public void testFindByDataFailing() {
-        setupSimpleSpring();
-        final List<SpringTable> entities = dao.findByData("does not exist");
+        setupSimpleTable();
+        final List<SimpleTable> entities = dao.findByData("does not exist");
 
         assertTrue(CollectionUtils.isEmpty(entities));
     }
 
     @Test
     public void testFindById() {
-        setupSimpleSpring();
-        final SpringTable entity = dao.findOne(10001000l);
+        setupSimpleTable();
+        final SimpleTable entity = dao.findOne(10001000l);
         assertEquals(10001000, entity.getId().longValue());
         assertEquals("one thousand", entity.getData());
     }
 
     @Test
     public void testFindByIdFailing() {
-        setupSimpleSpring();
+        setupSimpleTable();
         assertNull(dao.findOne(999l));
     }
 
     @Test
     public void testIsEmpty() {
-        jdbc.update("delete from SPRING_TABLE");
+        jdbc.update("delete from SIMPLE_TABLE");
         em.clear();
 
         assertTrue(dao.isEmpty());
@@ -159,15 +162,15 @@ public class SimpleSpringTestTransactionTest {
 
     @Test
     public void testisNotEmpty() {
-        setupSimpleSpring();
+        setupSimpleTable();
         assertFalse(dao.isEmpty());
     }
 
     @Test
     public void testRemoveManaged() {
-        setupSimpleSpring();
+        setupSimpleTable();
 
-        final SpringTable st = dao.findOne(10001000l);
+        final SimpleTable st = dao.findOne(10001000l);
         assertNotNull("entity to delete must not be null", st);
 
         dao.delete(st);
@@ -175,51 +178,53 @@ public class SimpleSpringTestTransactionTest {
         em.flush();
 
         assertTrue("most not find deleted entity",
-                jdbc.queryForList("select id from SPRING_TABLE where id = 10001000").isEmpty());
+                jdbc.queryForList("select id from SIMPLE_TABLE where id = 10001000").isEmpty());
         assertEquals("must be one entry less", 2,
-                jdbc.queryForObject("select count(ID) from SPRING_TABLE", Integer.class));
+                jdbc.queryForObject("select count(ID) from SIMPLE_TABLE", Integer.class));
 
     }
 
     @Test
     public void testUpdateManaged() {
-        setupSimpleSpring();
+        setupSimpleTable();
 
-        final SpringTable st = dao.findOne(10001000l);
+        final SimpleTable st = dao.findOne(10001000l);
 
         st.setData("updated");
 
         // The call to save is not needed here, but left in to show that it is
         // also ok. See also testUpdateManagedWithoutMerge()
-        dao.saveAndFlush(st);
+        dao.save(st);
+        em.flush();
 
-        assertEquals("updated", jdbc.queryForObject("select data from SPRING_TABLE where id = 10001000", String.class));
+        assertEquals("updated", jdbc.queryForObject("select data from SIMPLE_TABLE where id = 10001000", String.class));
     }
 
     @Test
     public void testUpdateManagedWithoutMerge() {
-        setupSimpleSpring();
+        setupSimpleTable();
 
-        final SpringTable st = dao.findOne(10001000l);
+        final SimpleTable st = dao.findOne(10001000l);
 
         st.setData("updated");
 
         // no call to dao.save here!
+
         em.flush();
 
-        assertEquals("updated", jdbc.queryForObject("select data from SPRING_TABLE where id = 10001000", String.class));
+        assertEquals("updated", jdbc.queryForObject("select data from SIMPLE_TABLE where id = 10001000", String.class));
     }
 
     @Test
     public void testUpdateUnmanaged() {
-        final SpringTable st = new SpringTable();
+        final SimpleTable st = new SimpleTable();
         st.setId(10001000l);
         st.setData("updated");
 
         dao.save(st);
         em.flush();
 
-        assertEquals("updated", jdbc.queryForObject("select data from SPRING_TABLE where id = 10001000", String.class));
+        assertEquals("updated", jdbc.queryForObject("select data from SIMPLE_TABLE where id = 10001000", String.class));
     }
 
 }
